ea3c4e02e07edebf55ec556d0b61fa0bca650130
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-05-14 20:58:15 +0530

    init client

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const app = express()
-
-app.use(express.json())
+const cors = require("cors")
 
 require("dotenv").config()
 
 const mongoose = require("mongoose")
-const UserModel = require("./models/users");
+const UserModel = require("./models/users")
+
+app.use(express.json())
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("Database connected"))
