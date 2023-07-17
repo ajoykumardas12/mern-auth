20950dc4089e0369a4f43aa73f3ee48af65e7926
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-17 23:53:02 +0530

    restructure api

--- api/index.js
+++ api/index.js
@@ -1,128 +1,134 @@
-const express = require("express");
+import express from "express";
 const app = express();
-const cors = require("cors");
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
-require("dotenv").config();
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
 
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+// import mongoose from "mongoose";
 
-const mongoose = require("mongoose");
-const UserModel = require("./models/users");
-const { default: isEmail } = require("validator/lib/isEmail");
+// import { UserModel } from "./models/users.js";
+// const { default: isEmail } = require("validator/lib/isEmail");
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("Database connected"))
-.catch((err) => console.log("Database not connected", err))
+app.use("/api/users", userRoutes);
 
-const handleErrors = (err) => {
-  console.log(err.message, err.code);
+app.use(notFound);
+app.use(errorHandler);
 
-  let errors = {name: "", email: "", password: ""};
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => console.log("Database connected"))
+//   .catch((err) => console.log("Database not connected", err));
 
-  if(err.code === 11000){
-    errors.email = "Email is already registered";
-    return errors;
-  }
+// const handleErrors = (err) => {
+//   console.log(err.message, err.code);
 
-  if(err.message.includes("users validation failed")){
-    Object.values(err.errors).forEach(({properties}) => {
-      errors[properties.path] = properties.message;
-    });
-  }
+//   let errors = { name: "", email: "", password: "" };
 
-  return errors;
-}
+//   if (err.code === 11000) {
+//     errors.email = "Email is already registered";
+//     return errors;
+//   }
 
-app.get("/users", (req, res) => {
-  UserModel.find({})
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving users");
-    });
-});
+//   if (err.message.includes("users validation failed")) {
+//     Object.values(err.errors).forEach(({ properties }) => {
+//       errors[properties.path] = properties.message;
+//     });
+//   }
 
-app.get("/user-details", (req, res) => {
-  const token = req.headers["x-access-token"];
+//   return errors;
+// };
 
-  try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    const email = decodedToken.email;
+// app.get("/users", (req, res) => {
+//   UserModel.find({})
+//     .then((users) => {
+//       res.json(users);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error retrieving users");
+//     });
+// });
 
-    UserModel.findOne({"email": email})
-      .then((user) => {
-        res.json(user.name);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error retrieving users");
-      });
-  } catch(error) {
-    console.log(error);
-    res.status(400).json({"msg": "Invalid token"});
-  }
+// app.get("/user-details", (req, res) => {
+//   const token = req.headers["x-access-token"];
 
-});
+//   try {
+//     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+//     const email = decodedToken.email;
 
-app.post("/signup", async (req, res) => {
-  const user = req.body;
-  try {
-    const newUser = new UserModel(user);
-    await newUser.save();
+//     UserModel.findOne({ email: email })
+//       .then((user) => {
+//         res.json(user.name);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         res.status(500).send("Error retrieving users");
+//       });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(400).json({ msg: "Invalid token" });
+//   }
+// });
 
-    res.status(200).json(user);
-  } catch (err) {
-    const error = handleErrors(err);
-    res.status(400).json(error);
-  }
-});
+// app.post("/signup", async (req, res) => {
+//   const user = req.body;
+//   try {
+//     const newUser = new UserModel(user);
+//     await newUser.save();
 
+//     res.status(200).json(user);
+//   } catch (err) {
+//     const error = handleErrors(err);
+//     res.status(400).json(error);
+//   }
+// });
 
-app.post("/login", async(req,res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
+// app.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+//   console.log(email, password);
 
-  if(!isEmail(email)){
-    return res.status(400).json({ msg: 'Please enter a valid email' });
-  }else{
-    try {
-      let user = await UserModel.findOne({ "email": email });
+//   if (!isEmail(email)) {
+//     return res.status(400).json({ msg: "Please enter a valid email" });
+//   } else {
+//     try {
+//       let user = await UserModel.findOne({ email: email });
 
-      if (!user) {
-        return res.status(400).json({ msg: 'Email or password incorrect' });
-      }
+//       if (!user) {
+//         return res.status(400).json({ msg: "Email or password incorrect" });
+//       }
 
-      // Compare hash of password with password in db
-      const isMatch = await bcrypt.compare(password, user.password);
+//       // Compare hash of password with password in db
+//       const isMatch = bcrypt.compare(password, user.password);
 
-      if (!isMatch) {
-        return res.status(400).json({ msg: "Email or password incorrect" });
-      }
+//       if (!isMatch) {
+//         return res.status(400).json({ msg: "Email or password incorrect" });
+//       }
 
-      // Generate JWT token
-      const token = jwt.sign(
-        {
-          name: user.name,
-          email: user.email,
-        },
-        process.env.JWT_SECRET
-      )
+//       // Generate JWT token
+//       const token = jwt.sign(
+//         {
+//           name: user.name,
+//           email: user.email,
+//         },
+//         process.env.JWT_SECRET
+//       );
 
-      res.status(200).json({ msg: "Login successful", user: token });
-    }
-    catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
-  }
-})
+//       res.status(200).json({ msg: "Login successful", user: token });
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send("Server error");
+//     }
+//   }
+// });
 
 app.listen(3125, () => {
-    console.log("Up and running on port 3125")
-})
+  console.log("Up and running on port 3125");
+});
