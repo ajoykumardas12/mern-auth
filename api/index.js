f506122e7a9dac2f7b60e7b4fb16b73a35a807de
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-18 12:01:31 +0530

    auth protected routes middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -19,6 +20,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
