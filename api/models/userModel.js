c63f6ac3fb9b797bff9d9bf5e98804b45d5ac57c
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-18 11:41:45 +0530

    login and logout, destroy cookie

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -44,6 +44,10 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("users", userSchema);
 
 export default User;
